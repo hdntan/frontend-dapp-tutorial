97cdb7cf6d37567d75f2f8597f5e72ba62c070fa
= <=>
2024-08-22 15:13:38 +0700

    Create a Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { DAppProvider, MoonbaseAlpha } from '@usedapp/core';
+import { getDefaultProvider } from 'ethers';
 import './index.css'
+
+
+const config = {
+  readOnlyChainId: MoonbaseAlpha.chainId,
+  readOnlyUrls: {
+    [MoonbaseAlpha.chainId]: getDefaultProvider(
+      'https://rpc.api.moonbase.moonbeam.network'
+    ),
+  },
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </StrictMode>,
 )
